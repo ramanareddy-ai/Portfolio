4e8b636f5f923e21d4409d8f86a89d6b935ce48d
Ramana Reddy Karnati <ramanareddykarnati@Mac-4644.lan>
2025-08-16 22:39:22 -0400

    init: Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,36 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: base must match your repo name exactly (case-sensitive)
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  build: { outDir: 'dist' }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
